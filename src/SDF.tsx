8432ba64d02770ab632fa572aa41fcc1b210528d
Renan Machado <renan.machado@coinbase.com>
2023-12-21 18:03:07 -0300

    SDF of a circle ⊙ω⊙

--- src/SDF.tsx
+++ src/SDF.tsx
@@ -1,23 +1,46 @@
-import React from "react";
 import {
   Skia,
   Canvas,
   Fill,
   Shader,
   Uniforms,
+  vec,
+  useTouchHandler,
+  useValue,
+  useComputedValue,
 } from "@shopify/react-native-skia";
+import { Dimensions } from "react-native";
 
 import ShaderDotSKSL from "./SDF.sksl";
 
-const uniforms: Uniforms = {
-  colors: ["#dafb61", "#61dafb", "#fb61da", "#61fbcf"].map((c) =>
-    Skia.Color(c)
-  ),
-};
+const { width, height } = Dimensions.get("screen");
+
+const colors = ["#dafb61", "#61dafb", "#fb61da", "#61fbcf"].map((c) =>
+  Skia.Color(c)
+);
 
 export const SDF = () => {
+  const pointer = useValue(vec(width / 2, height / 2));
+
+  const onTouch = useTouchHandler({
+    onActive: (event) => {
+      pointer.current = vec(event.x, event.y);
+    },
+  });
+
+  const uniforms = useComputedValue<Uniforms>(
+    () => ({
+      colors,
+      radius: width / 3,
+      center: vec(width / 2, height / 2),
+      strokeWidth: 2,
+      pointer: pointer.current,
+    }),
+    [pointer]
+  );
+
   return (
-    <Canvas style={{ flex: 1 }}>
+    <Canvas style={{ flex: 1 }} onTouch={onTouch}>
       <Fill>
         <Shader source={ShaderDotSKSL} uniforms={uniforms} />
       </Fill>
